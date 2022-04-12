a6486e0b5ccf0ee491da60a33cb965a69996b531
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-04-12 00:55:35 -0300

    Modificacao seeds

--- buy-from-me-backend/prisma/seeds/users.ts
+++ buy-from-me-backend/prisma/seeds/users.ts
@@ -38,6 +38,60 @@ const users: Users[] = [
     password: '$2b$10$Wqd2FoGq/7Rk3BUVR1tcMuF6mbNYjy/6tIpw6IGdymEe2HXMfOECi',//fernanda_password
     addressId: 14,
   },
+  {
+    id: '15',
+    name: 'Pricila',
+    lastName: 'Vendedor',
+    email: 'pricila@email.com',
+    contact: 987654321,
+    password: '$2b$10$Wqd2FoGq/7Rk3BUVR1tcMuitl.SX32bmFLz5lvXYu7VF0V7NXdrTO',//pricila_password
+    addressId: 15,
+  },
+  {
+    id: '16',
+    name: 'Ricardo',
+    lastName: 'Bahia',
+    email: 'ricardo@email.com',
+    contact: 987654321,
+    password: '$2b$10$Wqd2FoGq/7Rk3BUVR1tcMuOR4r5ax7.OhT0pICGqnbWx5VF0A87tW',//ricardo_password
+    addressId: 16,
+  },
+  {
+    id: '17',
+    name: 'Eliana',
+    lastName: 'Vendedor',
+    email: 'eliana@email.com',
+    contact: 987654321,
+    password: '$2b$10$Wqd2FoGq/7Rk3BUVR1tcMuVULm5UlVaC/fXmgFRB9AXiGdYYMXOm6',//eliana
+    addressId: 17,
+  },
+  {
+    id: '18',
+    name: 'Pedro',
+    lastName: 'Comprador',
+    email: 'pedro@email.com',
+    contact: 987654321,
+    password: '$2b$10$Wqd2FoGq/7Rk3BUVR1tcMuF6mbNYjy/6tIpw6IGdymEe2HXMfOECi',//pedro_password
+    addressId: 18,
+  },
+  {
+    id: '19',
+    name: 'Ana',
+    lastName: 'Vendedor',
+    email: 'ana@email.com',
+    contact: 987654321,
+    password: '$2b$10$Wqd2FoGq/7Rk3BUVR1tcMuVULm5UlVaC/fXmgFRB9AXiGdYYMXOm6',//ana_password
+    addressId: 19,
+  },
+  {
+    id: '20',
+    name: 'Gabriel',
+    lastName: 'Comprador',
+    email: 'gabriel@email.com',
+    contact: 987654321,
+    password: '$2b$10$Wqd2FoGq/7Rk3BUVR1tcMuF6mbNYjy/6tIpw6IGdymEe2HXMfOECi',//gabriel_password
+    addressId: 20,
+  }
 ];
 
 async function usersSeed() {
